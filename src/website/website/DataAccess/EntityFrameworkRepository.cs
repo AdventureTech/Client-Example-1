249cf516436a2fdadb99bbb9eb880048ba6d9b23
Phil Ledgerwood <phil.ledgerwood@adventuretechgroup.com>
2012-03-01 11:54:07 -0600

    Data access

---
+++ src/website/website/DataAccess/EntityFrameworkRepository.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace website.DataAccess
+{
+    public class EntityFrameworkRepository<T> : IRepository<T> where T : class
+    {
+        protected DbContext DatabaseContext;
+
+        public EntityFrameworkRepository(DbContext databaseContext)
+        {
+            DatabaseContext = databaseContext;
+        }
+
+        protected DbSet<T> DbSet
+        {
+            get { return DatabaseContext.Set<T>(); }
+        }
+
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+    }
+}
---
+++ src/website/website/DataAccess/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace website.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+    }
+}
