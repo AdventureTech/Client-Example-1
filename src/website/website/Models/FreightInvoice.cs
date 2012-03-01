ddca0c0516c1b73e44a84d75d88065fd883635c1
Phil Ledgerwood <phil.ledgerwood@adventuretechgroup.com>
2012-03-01 11:12:44 -0600

    Added model classes
    Signed-off-by: Phil Ledgerwood <phil.ledgerwood@adventuretechgroup.com>

---
+++ src/website/website/Models/FreightInvoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace website.Models
+{
+    public class FreightInvoice
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int LocationId { get; set; }
+        public int CarrierId { get; set; }
+        public Carrier Carrier { get; set; }
+        public decimal Amount { get; set; }
+        public int CarrierInvoiceId { get; set; }
+        public CarrierInvoice CarrierInvoice { get; set; }
+    }
+}
